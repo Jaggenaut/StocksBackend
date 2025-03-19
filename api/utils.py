42dfdb78882d1323694e62342caa1c6f498ea08e
Aditya <adityasi@iitbhilai.ac.in>
2025-03-19 12:08:47 +0530

    feat: first login error fixed

--- api/utils.py
+++ api/utils.py
@@ -4,10 +4,10 @@ from api.supabase_client import supabase
 
 # Dependency to get current user from Supabase token
 def get_current_user(authorization: Optional[str] = Header(None)):
-    # if not authorization:
-    #     raise HTTPException(status_code=401, detail="Authorization header missing")
-    # token = authorization.replace("Bearer ", "")
-    # user = supabase.auth.get_user(token)
-    # if not user:
-    #     raise HTTPException(status_code=401, detail="Invalid token")
-    return '03b5d8fc-a7f1-43a5-9346-e870131751bf'
+    if not authorization:
+        raise HTTPException(status_code=401, detail="Authorization header missing")
+    token = authorization.replace("Bearer ", "")
+    user = supabase.auth.get_user(token)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return user.user.id
